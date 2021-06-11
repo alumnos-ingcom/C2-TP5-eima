f3237ffd5c5a773c6bb9e2170882036e26d44c4a
Gerchu-arq <80314528+Gerchu-arq@users.noreply.github.com>
2021-06-11 01:52:50 -0300

    Create tp5ej1.py
    empezando...

---
+++ tp5ej1.py
@@ -0,0 +1,18 @@
+  
+############################################################
+# Matias G. Quevedo - @Gerchu-arq
+# Eimi Saiz - @EimiSaiz
+# 1. Pares e impares, TP5, Unidad 3.
+# UNRN Andina - Introducción a la Ingenieria en Computación
+############################################################
+
+# Escribir una función que retorne True cuando un número entero es par y False cuando no lo sea, sin utilizar módulo.(%)
+
+from tp4ej1 import ingreso_numero  # uso la funcion ingreso_numero(mensaje) del ej. 1, tp4
+
+def prueba():
+    """Toda la interacción con el usuario va acá"""
+    pass
+
+if __name__ == "__main__":
+    prueba()
