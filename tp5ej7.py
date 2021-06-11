8a4b7246dd149d1d552a0560703d4e3669043639
Gerchu-arq <80314528+Gerchu-arq@users.noreply.github.com>
2021-06-11 05:04:15 -0300

    Update tp5ej7.py
    Agregue la funcion ingresar numero, para que acepte un numero decimal

--- tp5ej7.py
+++ tp5ej7.py
@@ -11,15 +11,28 @@ Implementar este ejercicio sin utilizar la función valor absoluto abs().
 
 Ejemplo La distancia entre -6.5 y 6.0 es 12.5'''
 
-from tp4ej1 import ingreso_numero  # uso la funcion ingreso_numero(mensaje) del ej. 1, tp4
-
+def ingreso_num(mensaje):
+    """
+    Esta funcion muestra un mensaje y agrega la # para indicar el ingreso
+    de un número.
+    """
+    num_decimal = input(mensaje + " #")
+    try:
+        num_decimal_3cifras = float("{0:.2f}".format(num_decimal))    # si es un numero con cifras decimales, lo acorta a dos cifras
+    except Exception as ex:
+# Ha habido una excepción <class 'TypeError'>
+        print("Ha habido una excepción", type(ex))
+    else:  #Entra en else, no ha ocurrido ninguna excepción
+        print("No ha ocurrido ninguna excepción")
+        return num_decimal_3cifras
+    
 def Distancia(nro_1, nro_2):
   distancia = ((nro_1 - nro_2)**2)**(1/2)
   return distancia
   
 def prueba():
-  n1 = ingreso_numero("Ingrese primer numero:")
-  n2 = ingreso_numero("Ingrese segundo numero:")
+  n1 = ingreso_num("Ingrese primer numero:")
+  n2 = ingreso_num("Ingrese segundo numero:")
   print(f'La distancia entre {n1} y {n2} es: {Distancia(n1, n2):.2f}')
   
 if __name__ == "__main__":
