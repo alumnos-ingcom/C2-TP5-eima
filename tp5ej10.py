2e0c4bba39a82f06b5af0b76b8fafd53fff903c0
Eimi Saiz <83459802+EimiSaiz@users.noreply.github.com>
2021-06-22 00:18:10 -0300

    Cambio de mayúsculas a minúsculas en funciones.
    Falta la segunda función.

--- tp5ej10.py
+++ tp5ej10.py
@@ -1,21 +1,20 @@
 ########################################################
 # Matías G. Quevedo - @Gerchu-arqv
 # Eimi Saiz - @EimiSaiz
-# TP 5|Eje 10. Texto binario
+# TP 5 - Eje. 10: Texto binario
 # UNRN Andina - Introducción a Ingeniería en Computación
 ########################################################
 
-def DecimalToBinary(Decimal): 
-    if(Decimal > 1): 
-        DecimalToBinary(Decimal//2) 
-    print(Decimal%2, end=' ')
+def decimal_to_binary(decimal): 
+    if(decimal > 1): 
+        decimal_to_binary(decimal//2) 
+    print(decimal%2, end=' ')
 
 def prueba():
-    Decimal = int(input("Ingrese un número en base 10.\n"))
+    decimal = int(input("Ingrese un número en base 10.\n"))
     print("Representación binaria:")
-    DecimalToBinary(Decimal)
+    decimal_to_binary(decimal)
     pass
 
 if __name__ == "__main__":
     prueba()
-    
